09408ae7582e737494df340df94ed018be9b3219
Čeněk Žid <zidcenek@fit.cvut.cz>
2019-11-27 21:09:53 +0100

    some changes

--- main.py
+++ main.py
@@ -5,43 +5,80 @@ from time import time
 from twisted.internet import reactor
 import pandas as pd
 from datetime import datetime
+from datetime import date
 import copy
 
 DEPTH = BinanceSocketManager.WEBSOCKET_DEPTH_5
+CURRENT_DATE = date.today()
+DIRECTORY = './data/'
+CURRENCIES = [
+    {'currency': 'LTCBTC', 'file': None}, 
+    {'currency': 'BTCUSDT', 'file': None},
+    {'currency': 'LTCUSDT', 'file': None}
+]
 
-def process_message(msg, currency='no currency'):
+
+def calculate_files(currencies):
+    tmp_list = []
+    for dct in currencies:
+        tmp = {}
+        tmp['currency'] = dct['currency']
+        tmp['file'] = open(DIRECTORY + tmp['currency'] + "-" + str(CURRENT_DATE),"a+")
+        tmp_list.append(tmp)
+    currencies, tmp_list = tmp_list, currencies
+    for dct in tmp_list:
+        if dct['file'] is not None:
+            print('wrong')
+            dct['file'].close()
+    return currencies
+
+
+def make_lambda(curr):
+    return lambda msg: process_message(msg, curr)
+
+
+def update_current_date():
+    CURRENT_DATE = date.today()
+
+
+def process_message(msg, currency=None):
+    if currency is None:
+        return None
     try:
+#         print(CURRENT_DATE)
+#         print(currency['currency'])
         msg['timestamp'] = time()
-        date = datetime.now()
-        fname = currency + "_" + str(date.year) + "_" + str(date.month) + "_" + str(date.day)
-        f = open(fname,"a+")
+        if not CURRENT_DATE == date.today():
+            update_current_date()
+            CURRENCIES = calculate_files(CURRENCIES)
         for item in msg.values():
-            f.write(str(item) + ";")
-        f.write("\n")
-        f.close()
+            currency['file'].write(str(item) + ";")
+        currency['file'].write("\n")
     except Exception as e:
         print(e)
     return None
+    
     
 def startClient():
     api_key = 'aKNYXtMDVEsVyJCMNwcLGpAG9KVK9d5V568QCRADjDn5kumLeN9joKhnA4zIpBPd'
     api_secret = 'ziY288jGcFEdOv81RoWrAbAUf08LKwxpJ2EmKt1M2pHlhcB9UCaciGXHeIcZVFCZ'
     return Client(api_key, api_secret)
 
-def startDepthSocketManager(client, currencies):
+
+
+
+def startDepthSocketManager(client):
     bm = BinanceSocketManager(client)
     queries = []
-#     for i in range(len(currencies)):
-#         print("curr", currencies[i])
-#         queries.append(bm.start_depth_socket(str(currencies[i]), lambda msg: process_message(msg, str(currencies[i])), depth=DEPTH))
-    queries.append(bm.start_depth_socket(currencies[0], lambda msg: process_message(msg, currencies[0]), depth=DEPTH))
-    queries.append(bm.start_depth_socket(currencies[1], lambda msg: process_message(msg, currencies[1]), depth=DEPTH))
+    for currency in CURRENCIES:
+        queries.append(bm.start_depth_socket(currency['currency'], make_lambda(currency), depth=DEPTH))
     bm.start()
     return bm, queries
-    
+
+
 try:
-    currencies = ['LTCBTC', 'BNBBTC']
+    CURRENCIES = calculate_files(CURRENCIES)
     client = startClient()
-    socket_manager, queries = startDepthSocketManager(client, currencies)
+    socket_manager, queries = startDepthSocketManager(client)
 except Exception as e:
     display(e)
