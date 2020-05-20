a3e203c1a336bc485825f5f8b2d07c03629661ec
Čeněk Žid <zidcenek@fit.cvut.cz>
2020-05-20 07:44:05 +0200

    Sync with server version

--- data_collection/getbook.py
+++ data_collection/getbook.py
@@ -7,12 +7,14 @@ from datetime import datetime
 from datetime import date
 import copy
 import pandas as pd
+import sys
+from pathlib import Path
 
 class BinanceWebsocket():
-    def __init__(self):
+    def __init__(self, directory):
         self.DEPTH = BinanceSocketManager.WEBSOCKET_DEPTH_5
         self.CURRENT_DATE = date.today()
-        self.DIRECTORY = './data/'
+        self.DIRECTORY = directory
         self.CURRENCIES = []
         symbols = pd.read_csv("./relevant_symbols", header=None)
         for symbol in symbols[0]:
@@ -20,21 +22,32 @@ class BinanceWebsocket():
     
     
     def calculate_files(self):
-        # todo - přepsat 
+        '''
+            Calculates a path and creates a file for every currency pair in relevant_symbols file.
+        '''
         tmp_list = self.CURRENCIES
         for i in range(len(self.CURRENCIES)):
             self.CURRENCIES[i]['file'] = open(self.DIRECTORY + self.CURRENCIES[i]['currency'] + "-" + str(self.CURRENT_DATE),"a+")
 
             
     def make_lambda(self, curr):
+        '''
+            Makes a labda function reffering to prcess_message function
+        '''
         return lambda msg: self.process_message(msg, curr)
     
     
     def update_current_date(self):
+        '''
+            Updates date every day.
+        '''
         self.CURRENT_DATE = date.today()
 
 
     def process_message(self, msg, currency=None):
+        '''
+            Takes care of a timestamp addition and data collection.
+        '''
         if currency is None:
             return None
         try:
@@ -42,15 +55,20 @@ class BinanceWebsocket():
             if not self.CURRENT_DATE == date.today():
                 self.update_current_date()
                 self.calculate_files()
+            tmp = ""
             for item in msg.values():
-                currency['file'].write(str(item) + ";")
-            currency['file'].write("\n")
+                tmp += str(item) + ';'
+            tmp += '/n'
+            currency['file'].write(tmp)
         except Exception as e:
             print(e)
         return None
 
 
-    def startClient(self):
+    def start_client(self):
+        '''
+            Starts a client communicating with the Binance exchange
+        '''
         with open('apikey.txt', 'r') as apikey:
             api_key = apikey.read()
         with open('privatekey.txt', 'r') as privatekey:
@@ -58,7 +76,10 @@ class BinanceWebsocket():
         return Client(api_key, api_secret)
 
 
-    def startDepthSocketManager(self, client):
+    def start_depth_socket_manager(self, client):
+        '''
+            Starts a websocket for every currency pair
+        '''        
         bm = BinanceSocketManager(client)
         queries = []
         for currency in self.CURRENCIES:
@@ -66,12 +87,25 @@ class BinanceWebsocket():
         bm.start()
         return bm, queries
 
-try:
-    binance = BinanceWebsocket() 
-    binance.calculate_files()
-    client = binance.startClient()
-    socket_manager, queries = binance.startDepthSocketManager(client)
-    print('init end')
-except Exception as e:
-    print(e)
-    
+def initialize():
+    if len(sys.argv) != 2:
+        print("Please enter an output directory path as a first argument")
+        return 0;
+    output_directory = sys.argv[1]
+    if output_directory[-1] != '/':
+        output_directory += '/'
+    Path(output_directory).mkdir(parents=True, exist_ok=True)
+
+    try:
+        binance = BinanceWebsocket(output_directory) 
+        binance.calculate_files()
+        client = binance.start_client()
+        socket_manager, queries = binance.start_depth_socket_manager(client)
+        print('init end')
+    except Exception as e:
+        print(e)
+   
+   
+module_logger = logging.getLogger('log_test')   
+
+initialize()
